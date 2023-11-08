0ea1106f53bebcb293937e058bf91770d5925b23
thanigaivel-23 <23thanigaivel@gmail.com>
2023-11-08 23:40:28 +0530

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,14 @@ import Navbar from './home/Navbar'
 // import Data from './home/Data'
 // import Gauge from './home/Gauge'
 import Footer from './home/Footer'
-import LineChart from './LineChart'
+// import LineChart from './LineChart'
 
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
-        <LineChart/>
+        {/* <LineChart/> */}
         {/* <div><Data/></div> */}
         {/* <div className='md:mt-20'><Gauge/></div> */}
         <div className='text-gray-500 text-sm my-16'><Footer/></div>
